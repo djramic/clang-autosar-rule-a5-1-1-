df07a91d7fc964a166a6197ad39a6980040ea5e4
Djordje <djramic@gmail.com>
2022-12-20 16:59:20 +0100

    ::rotating_light:: template exsample test

--- clang/test/AutosarTest/template.cpp
+++ clang/test/AutosarTest/template.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 template<typename T>
 T myMax(T x, T y){
   return (x > y) ? 3 : 6;
@@ -7,7 +5,7 @@ T myMax(T x, T y){
 
 int main() {
 
-  std::cout<< myMax<int>(4, 7) << "\n";
-  std::cout<< myMax<char>('a','g')<<"\n";
-
+  myMax<int>(4, 7);
+  myMax<char>('a','g');
+  
 }
